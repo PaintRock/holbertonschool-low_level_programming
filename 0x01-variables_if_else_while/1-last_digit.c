9550271de6f372cffcf109379213376def5cbbff
adel <6126.holbertonstudents.com>
2023-01-27 17:13:51 -0800

    betty_starts_with_a_B

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -13,20 +13,20 @@ int n, dog;
 
 srand(time(0));
 n = rand() - RAND_MAX / 2;
-dog = n % 10;
+
 /* your code goes there */
 
-if (n < 5)
+if ((n % 10) > 5)
 {  
-printf("Last digit of %d is %d and is greater than 5\n", n, dog);
+printf("Last digit of %d is %d and is greater than 5\n", n, n % 10);
 }
-else if (dog < 6 && dog != 0)
+else if ((n % 10) < 6 && (n % 10) != 0)
 {
-printf("Last digit of %d is %d and is less than 6 and not 0\n", n, dog);
+printf("Last digit of %d is %d and is less than 6 and not 0\n", n, n % 10);
 }
 else
 {
-printf("Last digit of %d is %d and is 0\n", n, dog);
+printf("Last digit of %d is %d and is 0\n", n, n % 10);
 }
 return (0);
 }
