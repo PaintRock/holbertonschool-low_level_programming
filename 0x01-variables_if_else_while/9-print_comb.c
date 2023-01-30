472eea352871ba5f4e3137a7bb8614a9ffc1edc8
adel <6126.holbertonstudents.com>
2023-01-30 15:33:00 -0800

    one

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Prints the alphabet in lowercase.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+int p[];
+for (p  = 0; p < 10; p++)
+putchar((p % 10) + '0,');
+putchar('\n');
+return (0);
+}
