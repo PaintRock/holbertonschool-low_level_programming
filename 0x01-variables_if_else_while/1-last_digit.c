91449fa5db2cbbf2a79c65fea3fcea81f8461b1b
adel <6126.holbertonstudents.com>
2023-01-31 12:15:08 -0800

    darndog

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -9,7 +9,7 @@
  */
 int main(void)
 {
-int n, dog;
+int n;
 
 srand(time(0));
 n = rand() - RAND_MAX / 2;
@@ -17,7 +17,7 @@ n = rand() - RAND_MAX / 2;
 /* your code goes there */
 
 if ((n % 10) > 5)
-{  
+{
 printf("Last digit of %d is %d and is greater than 5\n", n, n % 10);
 }
 else if ((n % 10) < 6 && (n % 10) != 0)
