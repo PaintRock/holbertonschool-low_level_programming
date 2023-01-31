9114f6f2a79ca5b94a3dc6ab3435417908c33778
adel <6126.holbertonstudents.com>
2023-01-30 17:55:00 -0800

    why

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -6,9 +6,15 @@
  */
 int main(void)
 {
-int p[];
+int p;
 for (p  = 0; p < 10; p++)
-putchar((p % 10) + '0,');
+{
+putchar((p % 10) + '0');
+if (p == 9)
+continue;
+putchar(',');
+putchar(' ');
+}
 putchar('\n');
 return (0);
 }
