c363ad899677da856cb05803ef6e92ab053a52c2
adel <6126.holbertonstudents.com>
2023-02-02 04:54:13 -0800

    j

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,24 +1,19 @@
-#include <stdio.h>
-
+#include <ctype.h>
 /*
 *File: 4-isalpha.c
-*main - check the code.
+*
 *Auth:  A. Adel Knode
 */
-
-
 #include "main.h"
-
 /**
 * int _islower(int c); Finds lowercase 'c'.
 *
-* Return: Not Always 0.
+* Return: 1 if int c is an alphabet character
+* and 0 if other.
 */
-int main(void)
+int _isalpha(int c)
 {
-int c;
-if (c <= 'a' && c >= 'z')
-if (c <= 'A' && c >= 'Z')
+	if (((c <= 'a') && (c >= 'z')) || ((c <= 'A') && (c >= 'Z')))
 return (1);
 else
 return (0);
