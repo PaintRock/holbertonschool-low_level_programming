947c0a15bf1602afd034f71b305cf68b14c0460d
adel <6126.holbertonstudents.com>
2023-02-02 09:58:17 -0800

    ho

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,11 +7,14 @@
 void print_alphabet_x10(void)
 {
 	int c = 0;
-char l;
-while (c++ <= 9)
+	char l;
 
-for (l = 'a'; l <= 'z'; l++)
-putchar(l);
-
-putchar('n');
+	while (c++ <= 9)
+	{
+		for (l = 'a'; l <= 'z'; l++)
+		{
+			putchar(l);
+		}
+		putchar('\n');
+	}
 }
