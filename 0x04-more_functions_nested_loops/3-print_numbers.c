3befbe1860cce89e83f13f847cfa2a2820f8c1ab
adel <6126.holbertonstudents.com>
2023-02-03 20:13:45 -0800

    h

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -4,13 +4,12 @@
  * followed by a new line
  * Return: 0 Always. SUCCESS
  */
-void print_numbers(void);
+void print_numbers(void)
 {
-	int = i;
-	for (i >= 47; i <= 58; i++)
+	int i;
+	for (i = 0; i <= 9; i++)
 	{
-		_putchar(i);
+		_putchar('0' + i);
 	}
 	_putchar('\n');
-	return (0);
 }
