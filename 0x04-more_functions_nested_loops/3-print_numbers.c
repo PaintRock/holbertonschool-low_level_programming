b6fd08040ad34d7b590119dae723e5138d52095f
adel <6126.holbertonstudents.com>
2023-02-05 10:34:18 -0800

    x3

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -4,10 +4,11 @@
  */
 void print_numbers(void)
 {
-	int numi;
-	for (numi = 0; numi <= 9; numi++)
-	{
-		_putchar ('0' + numi);
-	}
-	_putchar('\n');
+int numi = 0;
+while (numi <= 9)
+{
+_putchar ('0' + numi);
+numi++;
+}
+_putchar('\n');
 }
---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ *print_most_numbers - print all but 2 and 4
+ *
+ * Return: void
+ */
+void print_most_numbers(void)
+{
+	int i;
+	for (i = 0; i <= 9; i++)
+	{
+if ((i = 2) || (i = 4))
+			_putchar ' ';
+	}
+else
+	_putchar i;
+_putchar ('\n');
+	}
+		   
