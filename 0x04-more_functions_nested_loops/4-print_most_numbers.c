48d070ce35a071aaed9419dacdf02edf901e862c
adel <6126.holbertonstudents.com>
2023-02-06 10:41:24 -0800

    space

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -7,13 +7,11 @@
 void print_most_numbers(void)
 {
 	int i;
+
 	for (i = 0; i <= 9; i++)
 	{
-if ((i = 2) || (i = 4))
-			_putchar ' ';
+		if (i != 2 && i != 4)
+			_putchar ((i % 10) + '0');
 	}
-else
-	_putchar i;
-_putchar ('\n');
-	}
-		   
+	_putchar ('\n');
+}
