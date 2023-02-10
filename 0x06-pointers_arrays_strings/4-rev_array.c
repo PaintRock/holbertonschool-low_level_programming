89bf9d20c61c50c4c4c4e48b771976960861ca45
adel <6126.holbertonstudents.com>
2023-02-09 18:31:29 -0800

    two_for_one

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -8,10 +8,12 @@ void reverse_array(int *a, int n)
 {
 	int tmp, i;
 
-	for (i = n - 1; i >= n / 2; i--)
+	n--;
+	for (i = 0; i < n; i++)
 	{
-		tmp = a[n - 1 - i];
-		a[n - 1 - i] = a[i];
-		a[i] = tmp;
+		tmp = a[i];
+		a[i] = a[n];
+		a[n] = tmp;
+		n--;
 	}
 }
--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,14 +1,48 @@
 #include "holberton.h"
-
 /**
- *cap_string Caps the words
- *@str: the string
- *Return: A pointer
+ *is_sep -- checks if char is a sep
+ *@s: the char to test
+ *
+ *Return: 1 or 0
  */
-char *cap_string(char *)
+int is_sep(char s)
 {
+	char sep[] = {'\t', '\n', ' ', ',', ';', '!',
+		      '.', '?', '\"', '(', ')', '{', '}'};
 	int i = 0;
 
-	while (str[i])
+	while (sep[i] != '\0')
+
 	{
-	       
+		if (s == sep[i])
+			return (1);
+		i++;
+	}
+	return (0);
+}
+
+
+/**
+ *cap_string - Caps the words
+ *@s: the string
+ *Return: A pointer
+ */
+char *cap_string(char *s)
+{
+	int sep, i;
+
+	sep = 1;
+	i = 0;
+
+	while (s[i] != '\0')
+	{
+		if (sep == 1 && (s[i] >= 'a' && s[i] <= 'z'))
+		{
+			s[i] -= 32;
+			sep = 0;
+		}
+		sep = is_sep(s[i]);
+		i++;
+	}
+	return (s);
+}
