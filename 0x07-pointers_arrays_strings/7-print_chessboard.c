1f0325daba9eea325808c7407230acb6a52c60c9
adel <6126.holbertonstudents.com>
2023-02-09 22:22:36 -0800

    no_way

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+/**
+ *print_chessboard - a fx that prints the chessboard
+ *@a: array of eight
+ *
+ *Return: void.
+ */
+void print_chessboard(char (*a)[8])
+{
+	unsigned int i, b;
+
+	for (i = 0; i < 8; i++)
+	{
+		for (b = 0; b < 8; b++)
+		{
+			_putchar(*((*a + i * 8) + b));
+		}
+		_putchar('\n');
+	}
+}
