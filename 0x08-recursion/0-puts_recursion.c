265b60c2c71f6a053ed97f8467f97817096989b0
adel <6126.holbertonstudents.com>
2023-02-17 15:28:06 -0800

    lets_go

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -6,16 +6,17 @@
 */
 void _puts_recursion(char *s)
 {
-	int i;
-	i = 0;
+int i;
 
-	if (s[i] != '\0')
-	{
-       	_putchar(s[i]);
-	_puts_recursion(&s[i + 1]);
-	}
-	if (s[i] == '\0')
-	{
+i = 0;
+
+if (s[i] != '\0')
+{
+_putchar(s[i]);
+_puts_recursion(&s[i + 1]);
+}
+if (s[i] == '\0')
+{
 _putchar('\n');
-
+}
 }
