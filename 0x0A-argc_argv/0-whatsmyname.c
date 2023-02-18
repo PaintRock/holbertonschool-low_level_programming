fe2d748865718cf1c696b50aa6d374c5f50a5705
adel <6126.holbertonstudents.com>
2023-02-18 08:11:58 -0800

    onehalf

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - print the name -
+ * @argc: for got the name
+ * @argv: array
+ * Return: 0
+ */
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%s", argv[0]);
+	return (0);
+}
