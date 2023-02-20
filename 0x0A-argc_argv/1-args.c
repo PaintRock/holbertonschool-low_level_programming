c598025c61357f6a6cdb619f59120b308b1310f0
adel <6126.holbertonstudents.com>
2023-02-19 19:27:19 -0800

    can

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -5,8 +5,12 @@
  * @argv: array
  * Return: 0
  */
-int main(int argc, char __attribute__ ((unused)) *argv[])
+int main(int argc, char *argv[])
 {
-	printf("%d\n", argc);
+	int count;
+
+	if (argc > 1)
+		for (count = 1; count < argc; count++)
+			printf("%s\n", argv[count]);
 	return (0);
 }
