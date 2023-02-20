f6dae9fb71d157cc2c22a4356e2cbbfd44dac309
adel <6126.holbertonstudents.com>
2023-02-19 19:15:07 -0800

    dunno

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - print the number -
+ * @argc: for got the name
+ * @argv: array
+ * Return: 0
+ */
+int main(int argc, char __attribute__ ((unused)) *argv[])
+{
+	printf("%d\n", argc);
+	return (0);
+}
