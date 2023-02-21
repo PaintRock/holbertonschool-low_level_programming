6240fea2374eb9dc7bd9a709727ad88752d70e14
adel <6126.holbertonstudents.com>
2023-02-20 16:27:49 -0800

    one

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -6,11 +6,26 @@
 */
 int _sqrt_recursion(int n)
 {
+	int sqrt;
 
-	if (n < 1)
+	if (n < 0)
+		return (-1);
+	else if (n <= 1 && n > 0)
+		return n;
+if (!(n % 2))
+	{
+		if (n / 2 == 2 || n == 2)
+			return (2);
+		sqrt = _sqrt_recursion(n / 2);
+		if (n / sqrt == sqrt)
+			return (sqrt);
+		else if (sqrt > 1)
+			return (sqrt * 2);
+		return (-1);
+	}
+	else
+	{
 		return (0);
+	}
 
-
-
-
-}
+	}
