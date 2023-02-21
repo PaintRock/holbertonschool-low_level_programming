b5b21982e4fe1ecc0878f13e1b105fda0fe2889f
adel <6126.holbertonstudents.com>
2023-02-20 17:06:01 -0800

    nogcc

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -6,8 +6,15 @@
  */
 int is_prime_number(int n)
 {
-	if (n < 1)
+	int i;
+
+	if (n <= 1)
 		return (0);
-	_print_rev_recursion((s + 1));
-	_putchar(*s);
+	if (i == 1)
+		return (1);
+	else if (n % i == 0)
+		return (0);
+	else if (n % i != 0)
+		return (is_prime_number((n, i -1)));
+			return (0);
 }
