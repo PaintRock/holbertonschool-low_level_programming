c965c5d88c0022869df0f20e8dd15c3b8c228523
adel <6126.holbertonstudents.com>
2023-02-20 21:49:47 -0800

    mu

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -7,25 +7,26 @@
 *Return: 0 or array location maybe
 */
 char *create_array(unsigned int size, char c)
-
 {
-	unsigned int a;
-	char *array;
+	char *p;
+	unsigned int i = 0;
 
-	a = 0;
-
-	if (size <= 0)
-		return (0);
-
-	array = malloc(size);
-	if (array == 0)
-	printf("NULL");
-		return (0);
-
-	while (a < size)
+	if (size == 0)
 	{
-		array[a] = c;
-		a++;
+		return (NULL);
 	}
-	return (array);
-}
+	else
+	{
+		p = malloc(size * sizeof(char));
+		if (p == NULL)
+		{
+			return (NULL);
+		}
+		while (i < size)
+		{
+			p[i] = c;
+			i++;
+		}
+	}
+	return (p);
+}~
