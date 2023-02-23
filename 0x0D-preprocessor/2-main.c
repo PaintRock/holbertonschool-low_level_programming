ed18cd7122a6648b48da5e2205125e9d0ccd68b6
adel <6126.holbertonstudents.com>
2023-02-23 10:13:32 -0800

    cp main

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *main - returns the name of the file it was compiled from
+ *
+ *Return: void
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
---
+++ 0x0D-preprocessor/2-main.c~
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *main - returns the name of the file it was compiled from
+ *
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
