a38ee2cb4924caea2008708583a6341bfc990603
adel <6126.holbertonstudents.com>
2023-02-26 15:34:40 -0800

    hush

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,11 +1,9 @@
-#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
-
 /**
  * malloc_checked - returns a pointer
- *@b: returns a pointer
- *Return: a pointer to the allocated mem unless it fails then 98
+ * @b: returns a pointer
+ * Return: a pointer to the allocated mem unless it fails then 98
  */
 void *malloc_checked(unsigned int b)
 {
