86cc61fda421b6e1e1b1832cd7a43128f3992000
adel <6126.holbertonstudents.com>
2023-03-01 00:19:39 -0800

    dog

--- 0x0B-malloc_free/0x0E-structures_typedef/dog.h
+++ 0x0B-malloc_free/0x0E-structures_typedef/dog.h
@@ -1,7 +1,13 @@
 #ifndef DOG_H
 #define DOG_H
+#include <stdio.h>
+#include <stdlib.h>
 /**
- * stuct dog - defines a new type with the following elements
+ * struct dog - defines a new type with the following elements
+ *@name: name of the dog
+ *@age: age of the dog
+ *@owner: name of the owner
+ */
 struct dog
 {
 	char *name;
