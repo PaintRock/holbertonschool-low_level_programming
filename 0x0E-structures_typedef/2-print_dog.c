654c6100e86ba4cd9dfa1d06738fc5f1c3680237
adel <6126.holbertonstudents.com>
2023-03-02 14:34:20 -0800

    pu

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -11,6 +11,6 @@ void print_dog(struct dog *d)
 	if (d == NULL)
 		return;
 	printf("Name: %s\n", d->name ? d->name : "Name: nil\n");
-	printf("Age: %6f\n", d->age >= 0 ? d->age : "Age: nil\n");
+	printf("Age: %f\n", d->age >= 0 ? d->age : "Age: nil\n");
 	printf("Owner: %s\n", d->owner ? d->owner : "Owner: nil\n");
 }
