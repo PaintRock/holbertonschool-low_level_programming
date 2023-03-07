ba281ed2460265296c0b2cfd6dac820a0c2cedd4
adel <6126.holbertonstudents.com>
2023-03-06 17:32:57 -0800

    t

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -7,16 +7,18 @@
  */
 int sum_them_all(const unsigned int n, ...)
 {
-	va_list ap;
-	unsigned int i;
-		int sum = 0;
+va_list my_list;
+unsigned int c;
+int sum;
 
-	if (n == 0)
-		return (0);
-	va_start(ap, n);
-	for (i = 0; i < n; i++)
-		sum += va_arg(ap, int);
-
-	va_end(ap);
-	return (sum);
+if (n == 0)
+return (0);
+va_start(my_list, n);
+sum = 0;
+c = 0;
+while (c < n)
+sum += va_arg(my_list, int);
+c++;
+va_end(my_list);
+return (sum);
 }
