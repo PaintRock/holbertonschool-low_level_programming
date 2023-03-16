0009dac34a7ec1104175c4907a096a8be0e5efa8
adel <6126.holbertonstudents.com>
2023-03-16 10:51:03 -0700

    first try

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ *list_len -- a function that returns the number of elements in a linked list_t
+ *list
+ *@h: the elements, maybe
+ *
+ *Return: the number of elements
+ */
+size_t list_len(const list_t *h)
+{
+	int i;
+
+	for (i = 0; h; i++)
+		h = h->next;
+	return (i);
+
+}
