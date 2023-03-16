6b8ecd0d9de14fa904ae86cebb1ee5f60ab18022
adel <6126.holbertonstudents.com>
2023-03-16 10:37:06 -0700

    nope

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -8,16 +8,16 @@
  **/
 size_t print_list(const list_t *h)
 {
-	size_t i;
+size_t i = 0;
 
-	for (i = 0; h; i++)
-	{
-		if (h->str == NULL)
-			printf("[%d] %s\n", 0, "(nil)");
-
-		else
-			printf("[%d] %s\n", h->len, h->str);
-		h = h->next;
-	}
-	return (i);
+while (h != NULL)
+{
+	if (h->str == NULL)
+		printf("[%d] %s\n", 0, "(nil)");
+	else
+		printf("[%d] %s\n", h->len, h->str);
+	h = h->next;
+	i++;
+}
+return (i);
 }
