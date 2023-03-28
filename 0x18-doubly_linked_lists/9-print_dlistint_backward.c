a6271145812042cf145d1df8146973e71639528e
adel <6126.holbertonstudents.com>
2023-03-27 19:12:51 -0700

    hush

--- 0x18-doubly_linked_lists/9-print_dlistint_backward.c
+++ 0x18-doubly_linked_lists/9-print_dlistint_backward.c
@@ -1,13 +1,18 @@
 #include "lists.h"
-
 /**
- * print_dlistint_backward -- fx that prints the elements of a dlistint_t list
- * backward
- *@h: ad
+ *print_dlistint_backward -- fx that prints all the elements of a
+ *dlistint_t list
+ *@h: the elements?
  *Return: the number of nodes
  */
 size_t print_dlistint_backward(const dlistint_t *h)
 {
-	h++;
-	return (0);
+	size_t i;
+
+	for (i = 0; h; i--)
+	{
+		printf("%d\n", h->n);
+		h = h->prev;
+	}
+	return (i);
 }
