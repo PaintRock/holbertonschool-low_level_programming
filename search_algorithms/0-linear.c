c7683873ba4395bf6faf1074400260a5b60d3081
PaintRock <6126@holbertonstudents.com>
2023-12-15 19:27:48 +0000

    me

---
+++ search_algorithms/0-linear.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int linear_search(int *array, size_t size, int value) {
+    if (array == NULL) {
+        return -1;
+    }
+
+    for (size_t i = 0; i < size; ++i) {
+        printf("Value checked array[%lu] = [%d]\n", i, array[i]);
+        if (array[i] == value) {
+            return i;
+        }
+    }
+
+    return -1; // If value is not found
+}
+
+int main(void) {
+    int array[] = {
+        10, 1, 42, 3, 4, 42, 6, 7, -1, 9
+    };
+    size_t size = sizeof(array) / sizeof(array[0]);
+
+    printf("Found %d at index: %d\n\n", 3, linear_search(array, size, 3));
+    printf("Found %d at index: %d\n\n", 42, linear_search(array, size, 42));
+    printf("Found %d at index: %d\n", 999, linear_search(array, size, 999));
+    return 0;
+}
