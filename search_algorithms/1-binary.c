793526a75493f97d7d4e12763f268bc3c5e96f2d
PaintRock <6126@holbertonstudents.com>
2023-12-20 21:37:44 +0000

    whalestelltales

--- search_algorithms/1-binary.c
+++ search_algorithms/1-binary.c
@@ -2,7 +2,7 @@
 #include "search_algos.h"
 
 /**
- * binary_search -  A function that searches for a value 
+ * binary_search -  A function that searches for a value
  * in a sorted array of integers using the Binary search algorithm.
  * @array: A pointer to the first element of the array to search in.
  * @size: The number of elements in the array.
